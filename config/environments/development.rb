e05694e3a65b6c0b549aa7b56f3220ed735dc9f6
mau5atron <gabrielbetancourt96@outlook.com>
2018-10-28 22:32:23 -0400

    simple form / guard livereload / devise views + user model / sidekiq route / action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.active_job.queue_adapter = :sidekiq
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
